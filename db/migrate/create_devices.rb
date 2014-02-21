cb8d60d3e91c4226e0f015817c26841fa049d0b2
Israel De La Hoz <israeldelahoz@gmail.com>
2014-02-21 00:50:44 -0300

    change user_id to polymorphic owner reference

--- db/migrate/create_devices.rb
+++ db/migrate/create_devices.rb
@@ -7,7 +7,7 @@ class CreateDeviceAuthDevices < ActiveRecord::Migration
     create_table :device_auth_devices do |t|
       t.string :key, null: false # device id
       t.string :token, null: false
-      t.integer :user_id, null: true # devices that aren't logged in will have no user_id
+      t.references :owner, polymorphic: true
       t.text :details, default: "{}", null: false
       t.string :status, null: false, default: 'active'
 
